d02725c8ca269cf22848ae75e38a99e0f4432c03
Semenov Denis <denis.semenov.prog@gmail.com>
2018-08-12 12:56:42 +0500

    Created some classes for testing Router API

---
+++ src/Stuff.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react';
+
+class Stuff extends Component {
+    render() {
+        return (
+            <div>
+                <h2>Stuff</h2>
+                <p>This is a stuff list</p>
+
+                <ul>
+                    <li>One</li>
+                    <li>Two</li>
+                    <li>Three</li>
+                    <li>Four</li>
+                </ul>
+            </div>
+        );
+    }
+}
+
+export default Stuff;
